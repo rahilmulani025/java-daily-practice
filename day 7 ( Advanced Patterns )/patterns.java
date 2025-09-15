49b3f2ff9a82da00a455bbbd63c3faa84f2bd635
rahimmulani025 <Rahilmulani1208@gmail.com>
2025-09-15 14:00:00 +0530

    feat: Add day 7 (Advanced Patterns)

---
+++ day 7 ( Advanced Patterns )/patterns.java
@@ -0,0 +1,84 @@
+import java.util.*;
+    public class patterns{
+
+
+        //fuction to print a hollow rectrangle
+        public static void hollow_rectrangle(int totrows,int totclmn){
+            //outer loop
+            for(int i=1;i<=totrows;i++){
+                // inner - columns
+                for(int j=1;j<=totclmn;j++){
+                    //cel-(i,j)
+                    if(i==1 || i==totrows || j==1 || j==totclmn){
+                        //boundry cell
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+
+        // function to print Inverted and rotated half pyramid
+        //    *
+        //   **
+        //  ***
+        // ****
+        public static void pyramid(int n){
+            for (int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }}
+
+        // function for printitng inverted pyramid with numbers
+        public static void inverted_number_pyramid(int n){
+            for(int i=1;i<=n;i++){
+                //inner - numbers
+                for(int j=1;j<=n-i+1;j++){
+                    System.out.print(j);
+                }
+                System.out.println();
+            }}
+
+        //function to print floyds traingle
+            public static void floyd_pattern(int n){
+                int counter =1;
+                for(int i=1;i<=n;i++){
+                     for(int j=1; j<=i;j++){
+                        System.out.print(counter+" ");
+                        counter++;
+            }
+            System.out.println();
+            }}
+
+
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+
+            //calling the function hollow rectriangle
+            // System.err.println("Enter No. of rows");
+            // int rows=sc.nextInt();
+            // System.err.println("Enter no. of columns");
+            // int columns=sc.nextInt();
+            // hollow_rectrangle(rows,columns);
+
+            // //calling a function to print pyramid 
+            // pyramid(4);
+
+            // calling a function to print inverted half number pyramid
+            // inverted_number_pyramid(10);
+
+            //calling a function to print floyds trainagle pattern
+            // floyd_pattern(5);
+            // // // //
+        }
+    }
