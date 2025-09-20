9630b52b160f8f80d52b370b4a4e9b6223f4892a
Rahil Mulani <141037000+rahilmulani025@users.noreply.github.com>
2025-09-20 17:51:09 +0530

    feat:Commit

--- day 8 ( Arrays )/arrays.java
+++ day 8 ( Arrays )/arrays.java
@@ -20,6 +20,64 @@ public class arrays{
         return -1;
     }
 
+    //creating a function to find the largest number in a array
+    public static int Findlargest(int numbers[]){ 
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){ 
+            if(numbers[i]>largest){
+                largest=numbers[i];
+            }
+        }
+        return largest;
+    }
+
+    //creating a function to find the smallest number in the guven array
+    public static int Findsmallest(int numbers[]){ 
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0;i<numbers.length;i++){ 
+            if(numbers[i]<smallest){
+                smallest=numbers[i];
+            }
+        }
+        return smallest;
+    }
+
+    //creating a function for binary search
+    public static int Binarysearch(int numbers[],int key){
+        int start=0,end=numbers.length-1;
+        //compare
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            if(numbers[mid]<key){
+              start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    //creating a function to create an array
+    public static void reverse(int numbers[]){
+        int first=0,last=numbers.length-1;
+        while(first<last){
+            int temp=numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+
+            first++;
+            last--;
+        }
+        
+    }
+
+    //creating a function to find all possible pairs in a array
+    
+
     public static void main (String args[]){
         Scanner sc=new Scanner(System.in);
 
@@ -62,8 +120,25 @@ public class arrays{
         // else{
         //      System.out.print("key is at index"+index);
         // }
-       
 
+        //finding the largest number in the gIVEN ARRAY
+        // int numbers[]={1,2,6,5,3};
+        // System.out.println("the largest number is"+" "+Findlargest(numbers));
+
+        // // finding the smallest number
+        // System.out.println("the Smallest number is"+" "+Findsmallest(numbers));
+
+        //Binary search
+        // int numbers[]={2,3,4,6,8,10,12,14};
+        // int key=10;
+        // System.out.print("the key is on index"+" "+Binarysearch(numbers, key));
+
+        //reverse an array
+        // int numbers[]={1,2,3,4,5,6};
+        // reverse(numbers);
+        // for(int i=0;i<numbers.length;i++){
+        //     System.out.print(numbers[i]);
+        // }
 
     }
 }
