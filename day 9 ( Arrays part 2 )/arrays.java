51263e8fa5c392ba4ed8f1ee5a73354c8bc59817
Rahil Mulani <141037000+rahilmulani025@users.noreply.github.com>
2025-09-25 23:37:40 +0530

    Update arrays.java

--- day 9 ( Arrays part 2 )/arrays.java
+++ day 9 ( Arrays part 2 )/arrays.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
+// function to print max subarray sum
 public class arrays{
      public static void main (String args[]){
+
             Scanner sc=new Scanner(System.in); }}
 
-// start
+// start from here 
+// 
