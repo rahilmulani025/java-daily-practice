f2e2b11fa765587c1c99fb8772805198b777d959
Rahil Mulani <141037000+rahilmulani025@users.noreply.github.com>
2025-10-01 22:55:22 +0530

    Add files via upload

--- day 9 ( Arrays part 2 )/arrays.java
+++ day 9 ( Arrays part 2 )/arrays.java
@@ -1,17 +1,36 @@
 import java.util.*;
 
-//////////
-
-// function to print max subarray sum
 public class arrays{
-     public static void main (String args[]){
-
-            Scanner sc=new Scanner(System.in); }}
-
-//////// start from here ///////
-// using the function max subarray's sum
 
 
+  //function to print the maximum sub array of an array
+  //brute force
+  public static void maxsubarraySum(int numbers[]){
+    int currSum =0;
+    int maxSum=Integer.MIN_VALUE;
 
+    for(int i=0;i<numbers.length;i++){
+      int start = i;
+      for(int j=i;j<numbers.length;j++){
+        int end =j;
+        currSum=0;
+        for(int k=start;k<=end;k++){
+          currSum += numbers[k];
+        }
+        System.out.println(currSum);
+        if(maxSum<currSum){
+          maxSum=currSum;
+        }
+        }
+      }
+      System.out.println("max sum="+maxSum);
+    }
+  
 
-
+  public static void main (String args[]){
+            Scanner sc=new Scanner(System.in); 
+          
+            int numbers[]={2,3,4,6,8,10};
+            maxsubarraySum(numbers);
+          
+          }}
