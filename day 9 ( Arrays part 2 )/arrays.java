9a195931a0ca440c7d4e14948cf3ae5367b638e1
Rahil Mulani <141037000+rahilmulani025@users.noreply.github.com>
2025-10-02 23:21:34 +0530

    Update arrays.java

--- day 9 ( Arrays part 2 )/arrays.java
+++ day 9 ( Arrays part 2 )/arrays.java
@@ -5,26 +5,26 @@ public class arrays{
 
   //function to print the maximum sub array of an array
   //brute force
-  public static void maxsubarraySum(int numbers[]){
-    int currSum =0;
-    int maxSum=Integer.MIN_VALUE;
+  // public static void maxsubarraySum(int numbers[]){
+  //   int currSum =0;
+  //   int maxSum=Integer.MIN_VALUE;
 
-    for(int i=0;i<numbers.length;i++){
-      int start = i;
-      for(int j=i;j<numbers.length;j++){
-        int end =j;
-        currSum=0;
-        for(int k=start;k<=end;k++){
-          currSum += numbers[k];
-        }
-        System.out.println(currSum);
-        if(maxSum<currSum){
-          maxSum=currSum;
-        }
-        }
-      }
-      System.out.println("max sum="+maxSum);
-    }
+  //   for(int i=0;i<numbers.length;i++){
+  //     int start = i;
+  //     for(int j=i;j<numbers.length;j++){
+  //       int end =j;
+  //       currSum=0;
+  //       for(int k=start;k<=end;k++){
+  //         currSum += numbers[k];
+  //       }
+  //       System.out.println(currSum);
+  //       if(maxSum<currSum){
+  //         maxSum=currSum;
+  //       }
+  //       }
+  //     }
+  //     System.out.println("max sum="+maxSum);
+  //   }
   
 
   public static void main (String args[]){
@@ -33,4 +33,5 @@ public class arrays{
             int numbers[]={2,3,4,6,8,10};
             maxsubarraySum(numbers);
           
+
           }}
